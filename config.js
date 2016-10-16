d314f7e749fb82c8431bf4e358f935052c8c138e
Ryan Ledford <ryan.c.ledford@gmail.com>
2016-10-16 13:32:06 +0000

    finalized cloud9 branch - ready for Heroku deployment

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-   db: "mongodb://localhost/short_url",
+   db: "mongodb://url_admin:aDmin2581@ds021356.mlab.com:21356/rl_sandbox",
    urlCollection: "short_url",
-   autoIncCollection: "autoInc",
+   autoIncCollection: "auto_inc",
    port: process.env.PORT || 3000
 };
--- server.js
+++ server.js
@@ -7,7 +7,17 @@ require('./short.server.route')(app);
 require('./goto.server.route')(app);
 
 app.get('/', function (req, res) {
-   res.send("Welcome!");
+   //ugly... should have used html (at least)... sorry
+   res.write("Welcome!\n\nIn order to generate a short URL please use the following format\n\n");
+   res.write("https://fcc-api-project-rledford.c9users.io/short/<your address goes here>");
+   res.write("\n\nPlease make sure you include http:// or https:// in the address you provide");
+   res.write("\n\nExample Input");
+   res.write("\nhttps://fcc-api-project-rledford.c9users.io/short/http://freecodecamp.com");
+   res.write("\n\nReturns\nhttps://fcc-api-project-rledford.c9users.io/goto/2");
+   res.write("\n\nPaste the returned address into your browsers address bar");
+   res.write("\nand you will be directed to http://freecodecamp.com");
+   res.write("\n\nThanks for visiting,\n\nRyan");
+   res.end();
 });
 
 console.log("listening on port: " + config.port);
